918584845664c91a0be0562583cb1ba05f57381f
PPGOO <1436067162@qq.com>
2022-06-22 20:12:36 +0800

    创建工程

---
+++ cmake-build-debug/CMakeFiles/MicroServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MicroServer.dir/main.cpp.o"
+  "MicroServer"
+  "MicroServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MicroServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
